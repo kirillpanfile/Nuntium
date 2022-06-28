5c54537793fa7aceb1cb7349152b67f675dc120b
RasslaBones <94486831+RasslaBones@users.noreply.github.com>
2022-06-28 14:52:12 +0300

    Finished Home Section

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,22 @@
 <template>
-  <div>Home</div>
+  <div class="home__container home">
+    <img src="../assets/home-hero/hero-image.jpg" alt="">
+    <div class="home__block block">
+      <div class="block__feature">FEATURED ARTICLE</div>
+      <div class="block__title">World’s Most Dangerous Technology Ever Made.</div>
+      <div class="block__name">
+        <p>Ralph Hawkins</p>
+        <div class="block__dot"></div>
+        <p>May 7, 2019 (10 mins read)</p>
+      </div>
+      <div class="block__text">
+        Proident aliquip velit qui commodo officia qui consectetur dolor ullamco aliquip elit incididunt. Ea minim ex consectetur excepteur. Ex laborum nostrud mollit sint consectetur Lorem amet aliqua do enim. Commodo duis dolor anim excepteur. In aliquip mollit nulla consequat velit magna.
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style></style>
