4500cf5cd4fe201e378af73d17d75f93d9068338
RasslaBones <94486831+RasslaBones@users.noreply.github.com>
2022-06-28 17:34:36 +0300

    HomeCard

--- src/components/UI/HomeCard.vue
+++ src/components/UI/HomeCard.vue
@@ -1,8 +1,18 @@
 <template>
   <div class="card__wrapper card">
-    <img :src="require(`../../assets/posts/${featuredPost[0].image}`)" alt="" />
+    <img :src="require(`../../assets/posts/${featuredPost[0].image}`)" alt="Error" />
+    <div class="card__desc desc">
+        <div class="desc__tag">{{ featuredPost[0].tag }}</div>
+        <div class="desc__title">{{ featuredPost[0].title }}</div>
+        <div class="desc__person person">
+            <div class="person__author">{{ featuredPost[0].author }}</div>
+            <div class="person__circle"></div>
+            <div class="person__date">{{ featuredPost[0].date }}</div>
+            <!-- 3 mins read -->
+        </div>
+        <div class="desc__description">{{ featuredPost[0].description }}</div>
+    </div>
   </div>
-  {{ featuredPost[0].image }}
 </template>
 
 <script>
@@ -12,7 +22,6 @@ export default {
   computed: {
     ...mapState(["posts"]),
     featuredPost() {
-      console.log(this.posts.sort((a, b) => b.views - a.views).slice(0, 1));
       return this.posts.sort((a, b) => b.views - a.views).slice(0, 1);
     },
   },
