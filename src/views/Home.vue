ff6bc3ae78331018da63fe921be429172987ddd3
RasslaBones <94486831+RasslaBones@users.noreply.github.com>
2022-06-28 15:41:46 +0300

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -39,11 +39,11 @@ export default {
         this.text = 'Proident aliquip velit qui commodo officia qui consectetur dolor ullamco aliquip elit incididunt. Ea minim ex consectetur excepteur. Ex laborum nostrud mollit sint consectetur Lorem amet aliqua do enim. Commodo duis dolor anim excepteur.'
         return this.text
       }
-      else if(this.DefaultWidth < 500 && this.DefaultWidth > 400){
+      else if(this.DefaultWidth <= 500 && this.DefaultWidth > 400){
         this.text = 'Proident aliquip velit qui commodo officia qui consectetur dolor ullamco aliquip elit incididunt. Ea minim ex consectetur excepteur. Ex laborum nostrud mollit sint consectetur Lorem amet aliqua do enim.'
         return this.text
       }
-      else if(this.DefaultWidth < 400){
+      else if(this.DefaultWidth <= 400){
         this.text = 'Proident aliquip velit qui commodo officia qui consectetur dolor ullamco aliquip elit incididunt. Ea minim ex consectetur excepteur.'
         return this.text
       }
