1d1122e44bb281af62908285b4f88e2484ef366e
RasslaBones <94486831+RasslaBones@users.noreply.github.com>
2022-06-29 10:52:38 +0300

    ScreenLock semi-finished

---
+++ src/views/ScreenLock.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="login login__wrapper">
+        <img :src="image" class="screenlock__img" alt="Error">
+        <login-input :placeholder="'Password'" type="password"></login-input>
+        <div class="login__options">
+            <div class="flex"> <!-- )), altfel nu stiam cum -->
+                <input type="checkbox" name="remember" id="remember">
+                <label for="remember">Remember me</label>
+            </div>
+            <a href="#" class="login__forgot">Forgot Password?</a>
+        </div>
+        <app-button scale="long">Login</app-button>
+    </div>
+</template>
+
+<script>
+import LoginInput from "@/components/UI/LoginInput"
+import AppButton from "@/components/UI/AppButton"
+export default{
+    name: "screenlock",
+    props:{
+        image: {
+            type: String,
+            required: false,
+            default: '../assets/icons/defaultUser.webp'
+        }
+    },
+    components: {
+        LoginInput,
+        AppButton
+    }
+}
+</script>
