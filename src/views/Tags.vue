777449a5184f90829041b3487571a75b51b76a62
RasslaBones <94486831+RasslaBones@users.noreply.github.com>
2022-06-29 15:21:13 +0300

    Update Tags.vue

--- src/views/Tags.vue
+++ src/views/Tags.vue
@@ -1,18 +1,37 @@
 <template>
     <div class="tags-page tags-page__wrapper">
         <div class="tags-page__field">
-            <input type="text" placeholder="Find the topics you care about..." />
+            <input v-model="currentTag" type="text" placeholder="Find the topics you care about..." />
             <img src="@/assets/icons/search.svg" alt="">
         </div>
-         <div class="tags-page__choices">
-            <div class="tags-page__variant" v-for="item in tags" :key="item">
-                #{{ item.toLowerCase() }}
+         <div class="tags-page__choices" v-if="currentTag === ''">
+            <div 
+                class="tags-page__variant" 
+                v-for="item in tags" 
+                :key="item"
+                @click="currentTag = item"
+                >#{{ item.toLowerCase() }}
             </div>
         </div>
+        <transition-group v-else name="list">
+            <HomeTagCard
+            v-for="card in tagPosts"
+            :key="card.id"
+            :title="card.title"
+            :description="card.description"
+            :author="card.author"
+            :date="card.date"
+            :image="card.image"
+            :tag="card.tag"
+          />
+        </transition-group>
     </div>
+    <button @click="check">123</button>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+import HomeTagCard from "@/components/UI/HomeTagCard";
 export default{
     name: "tags",
 
@@ -53,10 +72,19 @@ export default{
             "Life",
             "Food",
             "Sports",
-      ],
+            ],
+            currentTag: ''
         }
+    },
+
+    computed: {
+        ...mapGetters(["tagPosts"]),
+        check(){
+            console.log(this.tagPosts)
+        }
+    },
+    components:{
+        HomeTagCard
     }
-
-
 }
 </script>
