bf6234d987de0676057bb1c24f12e906c94d2273
RasslaBones <94486831+RasslaBones@users.noreply.github.com>
2022-06-30 16:25:10 +0300

    Finished About-Page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,64 +1,56 @@
 <template>
-    <div class="about about__wrapper about__margins">
-        <div class="about__logo">
-            <img src="@/assets/navbar/Logo.png" alt="" />
-        </div> 
-        <div class="about__desc">
-            A publishing company that focuses on the essentials.
+    <div class="about__hero hero">
+        <div class="hero__image">
+            <img src="@/assets/navbar/Logo-wide.png" alt="Error">
+        </div>
+        <div class="hero__text">A publishing company that focuses on the essentials.</div>
+    </div>
+
+    <div class="about__wrapper about">
+        <div class="about__block">
+            <div class="about__angel angel">
+                <div class="angel__desc">
+                    <div class="angel__title">We tell stories that drives the heart.</div>
+                    <div class="angel__text">Laboris consectetur sunt nulla eiusmod voluptate eiusmod dolor nisi qui dolor cillum fugiat ad. Id sit mollit labore sunt sit culpa qui minim pariatur et officia elit id. Tempor cupidatat veniam esse ad veniam dolore excepteur tempor dolor consectetur ut id.</div>
+                </div>
+                <div class="angel__image">
+                    <img src="@/assets/about-images/about-1.jpg" alt="Error">
+                </div>
+            </div>
+            <div class="about__woman woman">
+                <div class="woman__image">
+                    <img src="@/assets/about-images/about-2.jpg" alt="Error">
+                </div>
+                <div class="woman__desc">
+                    <div class="woman__title">We tell the news that makes the most impact.</div>
+                    <div class="woman__text">Laboris consectetur sunt nulla eiusmod voluptate eiusmod dolor nisi qui dolor cillum fugiat ad. Id sit mollit labore sunt sit culpa qui minim pariatur et officia elit id. Tempor cupidatat veniam esse ad veniam dolore excepteur tempor dolor consectetur ut id.</div>
+                </div>
+            </div>
+            <div class="about__share share">
+                <div class="share__title">We share the little moments that shows we’re alive.</div>
+                <div class="share__text">Laboris consectetur sunt nulla eiusmod voluptate eiusmod dolor nisi qui dolor cillum fugiat ad. Id sit mollit labore sunt sit culpa qui minim pariatur et officia elit id. Tempor cupidatat veniam esse ad veniam dolore excepteur tempor dolor consectetur ut id.</div>
+            </div>
+            <div class="about__carousel carousel">
+                <div class="carousel__image" v-for="item in images" :key="item">
+                    <img :src="require(`../assets/about-images/${item}`)" alt="Error" />
+                </div>       
+            </div>
         </div>
     </div>
 
-    <div class="about__block">
-        <div class="about__wrapper">
-            <div class="about__angel angel">
-                <div class="angel__block">
-                    <h3>We tell stories that drives the heart.</h3>
-                    <p>
-                        Laboris consectetur sunt nulla eiusmod voluptate eiusmod dolor nisi 
-                        qui dolor cillum fugiat ad. Id sit mollit labore sunt sit culpa 
-                        qui minim pariatur et officia elit id. Tempor cupidatat veniam esse 
-                        ad veniam dolore excepteur tempor dolor consectetur ut id.
-                    </p>
-                </div>
-                <div class="angel__block">
-                    <img src="../assets/about-images/about-1.jpg" alt="">
-                </div>
-            </div>
-    
-            <div class="about__doamna doamna">
-                <div class="doamna__image">
-                    <img src="@/assets/about-images/about-2.jpg" alt="">
-                </div>
-                <div class="doamna__desc">
-                    <h3>We tell the news that makes the most impact.</h3>
-                    <p>Laboris consectetur sunt nulla eiusmod voluptate eiusmod dolor nisi qui dolor cillum fugiat ad. Id sit mollit labore sunt sit culpa qui minim pariatur et officia elit id. Tempor cupidatat veniam esse ad veniam dolore excepteur tempor dolor consectetur ut id.</p>
-                </div>
-            </div>
-    
-            <div class="about__share share">
-                <h3>We share the little moments that shows we’re alive.</h3>
-                <p>Laboris consectetur sunt nulla eiusmod voluptate eiusmod dolor nisi qui dolor cillum fugiat ad. Id sit mollit labore sunt sit culpa qui minim pariatur et officia elit id. Tempor cupidatat veniam esse ad veniam dolore excepteur tempor dolor consectetur ut id.</p>
-            </div>
-    
-        </div>
-        <div class="about__colaj colaj">
-            <div class="colaj__image" v-for="item in images" :key="item">
-                <img :src="require(`../assets/about-images/${item}`)" alt="Error" >
-            </div>
-        </div>
-    </div>
-    <div class="about__human human">
-        <div class="human__title">Because we are you. Humans.</div>
-        <div class="human__descr">Laboris consectetur sunt nulla eiusmod voluptate eiusmod dolor nisi qui dolor cillum fugiat ad.</div>
+    <div class="about__humans humans">
+        <div class="humans__title">Because we are you. Humans.</div>
+        <div class="humans__text">Laboris consectetur sunt nulla eiusmod voluptate eiusmod dolor nisi qui dolor cillum fugiat ad.</div>
     </div>
 
     <div class="about__footer footer">
         <div class="footer__title">want to connect?</div>
-        <div class="footer__desc">Laboris consectetur sunt nulla eiusmod voluptate eiusmod dolor nisi qui..</div>
+        <div class="footer__text">Laboris consectetur sunt nulla eiusmod voluptate eiusmod dolor nisi qui..</div>
         <div class="footer__socials">
-            <img :src="require(`@/assets/icons/${item}`)" v-for="item in icons" :key="item" alt="Error">
+            <img :src="require(`@/assets/icons/${item}`)" v-for="item in icons" :key="item" alt="Error" />
         </div>
     </div>
+
 </template>
 
 <script>
@@ -66,7 +58,7 @@ export default{
     name: 'about',
     data(){
         return{
-            images: ['about-5.jpg','about-4.jpg','about-3.jpg',],
+            images: ['about-5.jpg','about-4.jpg','about-3.jpg'],
             icons: ['instagram.svg','twitter.svg','linkedIn.svg']
         }
     }
