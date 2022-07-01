fa6610008a0fa4bb5611e7ea24b498e703fa8c01
Panfile Kirill <95650362+kirillpanfile@users.noreply.github.com>
2022-07-01 15:01:43 +0300

    login end

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -2,12 +2,40 @@
   <div class="login login__wrapper">
     <div class="login__title">Welcome!</div>
     <div class="login__hero">Sign up to get the most out of nuntium.</div>
-    <login-input v-model.trim="email" :placeholder="'Email'" :type="'email'"></login-input>
-    <login-input v-model.trim="username" :placeholder="'Username'" :type="'username'"></login-input>
-    <login-input v-model="password" :placeholder="'Password'" :type="'password'"></login-input>
-    <app-button :scale="'long'" @click="check">Register</app-button>
-    <div class="login__signIn">Already have an account? 
-        <router-link to="/login" class="login__translate">Sign In</router-link>
+    <login-input
+      @email="
+        (data) => {
+          email = data;
+        }
+      "
+      v-model.trim="email"
+      :placeholder="'Email'"
+      :type="'email'"
+    ></login-input>
+    <login-input
+      v-model.trim="username"
+      :placeholder="'Username'"
+      :type="'username'"
+      @username="
+        (data) => {
+          username = data;
+        }
+      "
+    ></login-input>
+    <login-input
+      v-model="password"
+      :placeholder="'Password'"
+      :type="'password'"
+      @password="
+        (data) => {
+          password = data;
+        }
+      "
+    ></login-input>
+    <app-button :scale="'long'" @click="submit">Register</app-button>
+    <div class="login__signIn">
+      Already have an account?
+      <router-link to="/login" class="login__translate">Sign In</router-link>
     </div>
   </div>
 
@@ -21,24 +49,23 @@ import AppButton from "@/components/UI/AppButton";
 import LoginInput from "@/components/UI/LoginInput";
 export default {
   name: "Login",
-  data(){
-    return{
-        email: '',
-        username: '',
-        password: '',
-        registerUser: {
-            email: this.email,
-            username: this.username,
-            password: this.password
-        }
-    }
+  data() {
+    return {
+      email: "",
+      username: "",
+      password: "",
+    };
   },
-  methods:{
-    check(){
-        console.log(this.registerUser)
-        console.log(this.email);
-    }
-  },    
+  methods: {
+    submit() {
+      const createUser = {
+        username: this.username,
+        email: this.email,
+        password: this.password,
+      };
+      console.log(createUser);
+    },
+  },
 
   components: {
     AppButton,
