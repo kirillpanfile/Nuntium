26dae389cebeb09e7d5ef4937b015394c3b301b8
Panfile Kirill <95650362+kirillpanfile@users.noreply.github.com>
2022-07-01 17:36:28 +0300

    backend migrate

--- back/models/User.js
+++ back/models/User.js
@@ -18,7 +18,12 @@ const UserSchema = new mongoose.Schema(
     },
     profilePic: {
       type: String,
-      default: "",
+      default:
+        "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png",
+    },
+    jwt: {
+      type: String,
+      required: true,
     },
   },
   { timestamps: true }
